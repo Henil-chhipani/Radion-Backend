bf80d98b46bcd21d7bacf9ddebd672b09648a77a
Henil Chhipani <henilchhipani@gmail.com>
2023-12-20 19:25:47 +0530

    product schema building

---
+++ src/models/product.model.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+
+const prodcutSchema = new Schema(
+  {
+    product_id: {
+      type: String,
+      require: true,
+      unique: true,
+      index: true,
+    },
+    product_title: {
+      type: String,
+      require: true,
+    },
+    product_img: {
+      data: Buffer,
+      contentType: String,
+    },
+    product_discription:{
+        type: String,
+        require: true
+    },
+    product_price:{
+        type: Number,
+        require: true
+    },
+    product_category:{ //like electinic prodcut or fasion product
+        type: String,
+        require: true
+    },
+    product_brand:{
+        type: String,
+        require: true
+    },
+}
+ ,
+  { timestamps: true }
+);
+
+export const ProdcutSchema = mongoose.model("Product", prodcutSchema);
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,20 +1,39 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const userSchema = new mongoose.Schema(
+const userSchema = new Schema(
   {
-    FirstName: { type: String, require: true },
+    FirstName: { type: String, require: true, index: true },
+
     LastName: { type: String, require: true },
+
     DOB: { type: Date, require: true },
+
     Gender: { type: String, require: true },
+
     Email: { type: String, require: true },
-    Phone: { type: String, require: true, unique:false },
+
+    Phone: { type: String, require: true, unique: true, index: true },
+
     Address: { type: String, require: true },
+
     Password: { type: String, require: true },
+
     RePassword: { type: String, require: true },
+
     Blance: { type: Number, require: true },
+
+    previousPurchase: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "product",
+      },
+    ],
+
+    refershToken:{
+      type: String,
+    }
   },
   { timestamps: true }
 );
 
-export  const User = mongoose.model("User", userSchema);
-
+export const User = mongoose.model("User", userSchema);
