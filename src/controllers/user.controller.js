fb235ede896315783717d5ff6362ce8c89743d50
Henil Chhipani <henilchhipani@gmail.com>
2023-12-28 17:02:20 +0530

    Add register controller and debugging

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,49 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Apierror } from "../utils/ApiError.js";
+import { User } from "../models/user.models.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Radion server",
+  // get user data from form
+  // validation - not empty
+  //check if already exist: email
+  // create user object - create entry in db
+  // remove password and refresh token from response
+  // check user created or not
+  // return res
+  const { FirstName, LastName, DOB, Gender, Email, Phone, Password } = req.body;
+  console.log("email:", Email);
+
+  if (
+    [FirstName, LastName, DOB, Gender, Email, Phone, Password].some(
+      (field) => field?.trim() === ""
+    )
+  ) {
+    throw new Apierror(400, "all field are nessary");
+  }
+  const existedUser = User.findOne({
+    $or: [{ Email }, { Phone }],
   });
-}); 
-export {registerUser}
+  if (existedUser) {
+    throw new Apierror(409, "User with Email and Phone number already exists");
+  }
+  const user = await User.create({
+    FirstName,
+    LastName,
+    Email,
+    Phone,
+    Password,
+  });
+
+  const cratedUser = await User.findById(user._id).select(
+    "-Password, -refershToken"
+  );
+
+  if (!cratedUser) {
+    throw new Apierror(500, "Somethig went worng while registring the user");
+  }
+  return res.status(201).json(
+    new ApiResponse(200, cratedUser,"User register Successfully")
+  )
+});
+export { registerUser };
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,8 +1,9 @@
 class ApiResponse {
   constructor(statuscode, data, message = "sucess") {
     this.statuscode = statuscode;
-    this.data = data,
-    this.message = message
-    this.sucess = statuscode<400
+    (this.data = data), (this.message = message);
+    this.sucess = statuscode < 400;
   }
 }
+
+export { ApiResponse };
